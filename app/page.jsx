5d99b1398df8ddabed5ce77651215ffdf8b60359
brownstoneresearch <brownstonetberesearch@gmail.com>
2025-04-07 17:42:50 -0400

    Initial Netlify-ready Next.js  DApp

--- app/page.jsx
+++ app/page.jsx
@@ -1,26 +1,9 @@
 'use client';
 
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import WalletBalance from '../components/WalletBalance';
-import TransferForm from '../components/TransferForm';
-import StakingPanel from '../components/StakingPanel';
-import FloatingTransactions from '../components/FloatingTransactions';
-
 export default function Page() {
   return (
-    <div style={{
-      minHeight: '100vh',
-      padding: '2rem',
-      backgroundColor: '#000',
-      color: '#fff',
-      fontFamily: 'Inter, sans-serif'
-    }}>
-      <h1>$BURNA Token</h1>
-      <ConnectButton />
-      <WalletBalance />
-      <TransferForm />
-      <StakingPanel />
-      <FloatingTransactions />
-    </div>
+    <main style={{ color: 'white', backgroundColor: 'black', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h1>$BURNA Next.js DApp on Netlify</h1>
+    </main>
   );
 }
