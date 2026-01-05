e3088f36395a0abc4667851e45471a23435d45d7
brownstoneresearch <brownstonetberesearch@gmail.com>
2026-01-04 20:43:18 -0800

    Delete app directory

--- app/page.jsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function Page() {
-  return (
-    <main style={{ color: 'white', backgroundColor: 'black', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <h1>$BURNA Next.js DApp on Netlify</h1>
-    </main>
-  );
-}
